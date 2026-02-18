d2c622e6a5be43c2a65dcde1c24876ccf9ceb30c
arfan4-dev <arfan4.dev@gmail.com>
2026-02-18 12:03:26 +0500

    feat: Implement API endpoints and controller logic for push notification subscription management.

--- src/routes/notification.routes.js
+++ src/routes/notification.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { protect } from '../middleware/auth.middleware.js';
-import {subscribeUser} from '../controllers/notification.controller.js';
+import {subscribeUser, unsubscribeUser} from '../controllers/notification.controller.js';
 
 const router = express.Router();
 
 router.post('/subscribe', protect, subscribeUser);
+router.post('/unsubscribe', protect, unsubscribeUser);
 
 export default router;
